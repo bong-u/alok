bea39345e08343418faedbd65e5b031260f13925
bong-u <bongudev@gmail.com>
2024-12-07 14:52:17 +0900

    chore: 테스트 시 db truncate 수행

--- be/src/env.ts
+++ be/src/env.ts
@@ -1,6 +1,14 @@
-import fs from "fs";
 import { execSync } from "child_process";
 import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+const resetDatabase = async () => {
+	const prisma = new PrismaClient();
+	await prisma.$executeRawUnsafe(
+		`TRUNCATE TABLE member, date RESTART IDENTITY CASCADE;`
+	);
+	prisma.$disconnect();
+};
 
 if (!process.env.NODE_ENV) {
 	console.error("NODE_ENV is not set");
@@ -8,34 +16,15 @@ if (!process.env.NODE_ENV) {
 }
 
 if (process.env.NODE_ENV === "test") {
-	dotenv.config({ path: ".env.test" });
+	dotenv.config({ path: ".env.test", override: true });
 
-	const databaseUrl = process.env.DATABASE_URL;
-	if (!databaseUrl) {
+	console.log(process.env.DATABASE_URL);
+	if (!process.env.DATABASE_URL) {
 		console.error("DATABASE_URL is not set");
 		process.exit(1);
 	}
 
-	const dbPath = databaseUrl.startsWith("file:")
-		? databaseUrl.split("file:")[1]?.split("?")[0]
-		: null;
-
-	if (!dbPath) {
-		console.error("Invalid DATABASE_URL format");
-		process.exit(1);
-	}
-
-	// 데이터베이스 파일 삭제
-	if (fs.existsSync(dbPath)) {
-		console.log(`Deleting database file: ${dbPath}`);
-		fs.unlinkSync(dbPath);
-		console.log("Database file deleted.");
-	} else {
-		console.log(`Database file not found: ${dbPath}`);
-	}
-
-	// Prisma 스키마 재생성
-	console.log("Recreating database schema...");
+	resetDatabase();
 	execSync("npx prisma db push", { stdio: "inherit" });
 	console.log("Database recreated successfully.");
 } else {
