397a1df9c350c4e1a15ca02321acdd6f1b412f4d
bong-u <bongudev@gmail.com>
2024-12-09 11:15:44 +0900

    fix: 테스트 환경에서 사용하는 redis db 분리
    운영: 0번, 테스트: 1번

--- be/src/redis.ts
+++ be/src/redis.ts
@@ -4,6 +4,7 @@ const redis = new Redis({
 	host: process.env.REDIS_HOST,
 	port: Number(process.env.REDIS_PORT),
 	password: process.env.REDIS_PASSWORD || "",
+	db: process.env.NODE_ENV === "test" ? 1 : 0, // 테스트 환경이면 DB 1번, 아니면 DB 0번
 });
 
 export default redis;
